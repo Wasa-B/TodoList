513b2b299f7cfd6e010815706115e49786e9e494
Wasa-B <ghd1876@gmail.com>
2025-05-08 09:36:44 +0900

    removeData

--- app.js
+++ app.js
@@ -18,49 +18,95 @@ const localKey = 'todoList';
 const summit = document.querySelector('.summit-btn');
 const input = document.querySelector('.main input');
 const list = document.querySelector('.todo-box-list');
-const item = document.querySelector('.first-item');
+const item = document.querySelector('.template');
+// todolist 데이터 구조
 var listData = {
     list: [],
 };
+//todoData생성
+function createData(text) {
+    var _date = new Date();
+    var data = {
+        text: text,
+        date: _date.toDateString(),
+        done: false,
+    };
+    return data;
+}
+// 로컬데이터 저장
+function saveData() {
+    var data = JSON.stringify(listData);
+    localStorage.setItem(localKey, data);
+}
+// 로컬데이터 로드
 function loadData() {
     var data = localStorage.getItem(localKey);
     if (data == null) return;
     listData = JSON.parse(data);
 }
-loadData();
-function saveData() {
-    var data = JSON.stringify(listData);
-    localStorage.setItem(localKey, data);
+// todo data 추가
+function addData(data) {
+    listData.list.push(data);
 }
-
-function addList(title, date, done) {
-    listData.list.append([title, date, done]);
+// todo data 제거
+function removeData(index) {
+    listData.list.splice(index, 1);
 }
-
-function drawList() {}
-
-function generateTodo(text) {
+// todo data 변경
+function updateData(index, data) {
+    listData.list[index] = data;
+}
+// todoList 그리기
+// --template 새성
+function genElement(data, index) {
     var todo = item.cloneNode(true);
     todo.className = '';
     var titleBox = todo.querySelector('.title');
-    titleBox.innerText = text;
-
-    var date = new Date();
+    titleBox.innerText = data.text;
     var dateBox = todo.querySelector('.date');
-    dateBox.innerText = date.toDateString();
+    dateBox.innerText = data.date;
 
-    const delBtn = todo.addEventListener('click', function () {});
+    var isDone = todo.querySelector('.done-btn i');
+    if (data.done == true) {
+        isDone.className = 'ri-checkbox-line';
+    }
 
-    return todo;
+    isDone.addEventListener('click', function () {
+        updateData(index, createData(data.text, data.date, !data.done));
+        saveData();
+        drawList();
+        console.log(data);
+    });
+
+    var delBtn = todo.querySelector('.delete-btn');
+    delBtn.addEventListener('click', function () {
+        removeData(index);
+        drawList();
+        saveData();
+    });
+    list.append(todo);
 }
-
+// --전체 리스트 그리기
+function drawList() {
+    while (list.firstChild) {
+        list.firstChild.remove();
+    }
+    listData.list.forEach((item, index) => {
+        genElement(item, index);
+    });
+}
+loadData();
+drawList();
+//항목 추가 버튼 액션
 summit.addEventListener('click', function () {
     var text = input.value;
     if (text == '') {
         text = 'Empty';
     }
+
+    addData(createData(text));
+    drawList();
     input.value = '';
-    list.append(generateTodo(text));
     console.log(list.innerHTML);
     saveData();
 });
